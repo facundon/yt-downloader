ac9ac8eb5b73dd2cc6a15aa4341836ad753bc574
Facundo Rodriguez <facundonrodriguez@gmail.com>
2021-05-29 19:10:31 -0300

    Add Loader component Add disabled and loading props to button Add useForm to handle login form Add red color on sass variables

--- src/components/organisms/RegisterForm/index.tsx
+++ src/components/organisms/RegisterForm/index.tsx
@@ -1,41 +1,69 @@
+import { useForm } from "react-hook-form"
 import { Button, Input } from "../../atoms"
 
 import "./index.scss"
 
 type RegisterFormProps = {
    openLogin: () => void
+   close: () => void
+}
+type FormValues = {
+   name: string
+   email: string
+   password: string
+   repPassword: string
 }
 
-const RegisterForm: React.FC<RegisterFormProps> = ({ openLogin }) => {
+const RegisterForm: React.FC<RegisterFormProps> = ({ openLogin, close }) => {
+   const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { errors },
+   } = useForm<FormValues>()
+
+   const onSubmit = (values: FormValues) => {}
    return (
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
          <h2>Sign Up</h2>
          <div className="login-wrapper register">
             <Input
                icon="person"
                placeholder="Name"
                type="text"
-               name="name"
+               {...register("name", { required: true })}
                autoFocus
             />
+            {errors.name && (
+               <p className="input-error">{errors.name.message}</p>
+            )}
             <Input
                icon="alternate_email"
                placeholder="Email"
                type="email"
-               name="email"
+               {...register("email", { required: true })}
             />
+            {errors.email && (
+               <p className="input-error">{errors.email.message}</p>
+            )}
             <Input
                icon="vpn_key"
                placeholder="Password"
                type="password"
-               name="password"
+               {...register("password", { required: true })}
             />
+            {errors.password && (
+               <p className="input-error">{errors.password.message}</p>
+            )}
             <Input
                icon="vpn_key"
                placeholder="Repeat Password"
                type="password"
-               name="password"
+               {...register("password", { required: true })}
             />
+            {errors.repPassword && (
+               <p className="input-error">{errors.repPassword.message}</p>
+            )}
             <Button icon="how_to_reg" appareance="secondary" type="submit">
                Sign Up!
             </Button>
